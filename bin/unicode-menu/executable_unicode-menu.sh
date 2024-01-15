5c29d7c3746209cb8c9a91e98f05bb863dc87493
os <git@osiris.moe>
2024-01-15 22:51:41 +0100

    Added unicode menu script

---
+++ bin/unicode-menu/executable_unicode-menu.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+chosen=$(grep -h -v '^#' ~/bin/unicode-menu/{favorites,emoji}.csv | rofi -dmenu -p copy -i -l 20)
+
+[ "$chosen" != '' ] || exit 42
+
+c=$(echo "$chosen" | sed 's/ .*$//')
+printf "$c" | wl-copy --primary
+printf "$c" | wl-copy
+notify-send \
+    --urgency  low \
+    --icon     '/usr/share/icons/Papirus-Dark/22x22/actions/edit-copy.svg' \
+    --app-name 'Unicode Menu' \
+    'Unicode Menu' \
+    "'$c' copied to clipboard."
+
