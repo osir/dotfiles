10b3ac31454d4cdcef308f78e6daf9b94097126d
os <git@osiris.moe>
2024-04-23 18:31:37 +0200

    Added dir menu

---
+++ bin/dir-menu/executable_dir-menu.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# Select a directory in rofi and open it in a terminal
+
+declare -a lines
+while IFS= read -r line
+do
+    if [[ "$line" != *\;* || "$line" == \#* ]]
+    then
+        continue
+    fi
+    name="${line%%;*}"
+    path="${line#*;}"
+    if [[ "$name" == '*' ]]
+    then
+        for subdir in "${path/#~/$HOME}"/*
+        do
+            if [[ -d "$subdir" ]]
+            then
+                lines+=( "${subdir##*/}; $subdir" )
+            fi
+        done
+    else
+        lines+=( "$name; $path" )
+    fi
+done < ./dirs.csv
+
+chosen=$(
+    printf '%s\n' "${lines[@]}" \
+        | rofi -dmenu -p man -i -l 20
+)
+
+[ "$chosen" != '' ] || exit 0
+path="${chosen#*; }"
+
+kitty -d "${path/#~/$HOME}"
+
